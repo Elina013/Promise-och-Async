4b469dfae1ae56aad8da5b3a6caba87750532d8f
Elina013 <holmberg.lina@yahoo.se>
2023-10-15 21:33:47 +0200

    te4

---
+++ js/country-info.js
@@ -0,0 +1,8 @@
+const fetchData = async () => {
+    const res = await fetch("https://restcountries.eu/rest/v2/alpha/col");
+    const country = await res.json();
+    console.log(country);
+  };
+  
+  fetchData();
+  
---
+++ js/promise.js
@@ -0,0 +1,36 @@
+const onMyBirthday = (isKayoSick) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (!isKayoSick) {
+          resolve(2);
+        } else {
+          reject(new Error("I am sad"));
+        }
+      }, 2000);
+    });
+  };
+  
+  // If Kayo is not sick
+  onMyBirthday(false)
+    .then((result) => {
+      console.log(`I have ${result} cakes`); // In the console: I have 2 cakes
+    })
+    .catch((error) => {
+      console.log(error); // Does not run
+    })
+    .finally(() => {
+      console.log("Party"); // Shows in the console no matter what: Party
+    });
+  
+  // If Kayo is sick
+  onMyBirthday(true)
+    .then((result) => {
+      console.log(`I have ${result} cakes`); // does not run
+    })
+    .catch((error) => {
+      console.log(error); // in console: Error: I am sad
+    })
+    .finally(() => {
+      console.log("Party"); // Shows in the console no matter what: Party
+    });
+  
